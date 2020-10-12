2709be43cadcf665cec1255417eff8cdd889caaa
Ebenezer Don <ebenezerdonu@gmail.com>
2020-10-12 01:16:19 +0100

    add tests

---
+++ index.test.js
@@ -0,0 +1,22 @@
+const { calculateAge, updateDomWithAge } = require('./index')
+const { screen, fireEvent } = require('@testing-library/dom')
+
+test(`calculates a user's age`, () => {
+  expect(calculateAge(2015)).toBe(5)
+})
+
+test(`displays the user's age on form submit`, () => {
+  document.body.innerHTML = `
+    <form class="age-form">
+      <input type="number" class="year-of-birth" value=1993>
+      <div class="mt-2">
+        <button type="submit" class="btn btn-primary">Tell me</button>
+      </div>
+    </form>
+    <h3 class="result"></h3>
+  `
+
+  updateDomWithAge({preventDefault: () => {}})
+  const resultElement = document.querySelector('.result')
+  expect(resultElement.innerText).toBe(`You are 27 years old!`)
+})
