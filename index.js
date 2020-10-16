4e6dcc194756b6e82caa31a286e305b5ea0a9009
Ebenezer Don <ebenezerdonu@gmail.com>
2020-10-16 15:55:36 +0100

    get current year in updateDomWithAge function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const calculateAge = yearOfBirth => {
-  const currentYear = new Date().getFullYear()
+const calculateAge = (yearOfBirth, currentYear) => {
   const age = currentYear - yearOfBirth
   return age
 }
 
 const updateDomWithAge = event => {
   event.preventDefault()
+  const currentYear = new Date().getFullYear()
   const yearOfBirth = document.querySelector('.year-of-birth').value
-  const userAge = calculateAge(yearOfBirth)
+  const userAge = calculateAge(yearOfBirth, currentYear)
 
   const resultElement = document.querySelector('.result')
   resultElement.innerText = `You are ${userAge} years old!`
